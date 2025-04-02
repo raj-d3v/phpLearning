145a19db2ee33f72116bfeb1747e61d57013c1e3
Raj Patel <raj@d3vtech.com>
2025-04-02 13:19:48 +0530

    Add files via upload

---
+++ php-logical_operators/logical_operators.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Logical operators = combine conditional statements
+    // if(condition1 && condition2)
+
+    // && AND  = True if both conditions are true
+    // || OR   = True if at least one conditin is true
+    //  ! NOT  = True if false. False if true.
+
+
+?>
